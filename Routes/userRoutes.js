190f40173bb08f329a447bfada33524be7d31006
Prakhar <prakhar24680@gmail.com>
2020-06-28 22:28:21 +0530

    listProducts

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -13,7 +13,7 @@ Router.post('/userLogInPost', controllers.logInPost, cookieHandler.sendCookie)
 
 Router.get('/user/listShops', cookieHandler.cookieRecieved, controllers.listShops)
 
-Router.get('user/shops/listProducts', controllers.listProducts)
+Router.get('/user/shops/listProducts', controllers.listProducts)
 
 
 export default Router
